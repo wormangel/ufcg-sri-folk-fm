39864e5ae3c17486a21cf39503a638e08de04112
wormangel <wormangel@2af12e46-81bf-4599-da0b-a47663b5e3f1>
2011-06-08 03:42:30 +0000

    Criado aplicativo folkapp dentro do projeto folkfm. Coisas recolocadas no lugar.

---
+++ folk_fm/folkapp/models.py
@@ -0,0 +1,22 @@
+from django.db import models
+
+# Model
+
+class User(models.Model):
+	name = models.CharField(max_length=30)
+	friends = models.ManyToManyField(User)
+	
+	def tag(self, band, tagText):
+	
+	def addFriend(self, user):
+	
+class Tag(models.Model):
+	text = models.CharField(max_length=20)
+	
+class Band(models.Model):
+	name = models.CharField(max_length=40)
+
+class UserTagBand(models.Model):
+    user = models.ForeignKey(User)
+    tag = models.ForeignKey(Tag)
+    band = models.ForeignKey(Band)
