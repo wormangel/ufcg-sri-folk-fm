d28803506a8b066d7c33872135fe83bb0cd6fc74
wormangel <wormangel@2af12e46-81bf-4599-da0b-a47663b5e3f1>
2011-06-08 04:27:30 +0000

    Adicionadas paginas esqueleto.

--- folk_fm/folkapp/views.py
+++ folk_fm/folkapp/views.py
@@ -1,1 +1,17 @@
 # Create your views here.
+from django.http import HttpResponse
+
+def index(request):
+    return HttpResponse('This is the index (login?) page! :D')
+
+def user_profile(request, id_user):
+    return HttpResponse('This is the user profile page! :D')
+
+def band_profile(request, id_band):
+    return HttpResponse('This is the band profile page! :D')
+
+def bands_by_tag(request, id_tag):
+    return HttpResponse('This page lists every single band marked with a specific tag! :D')
+
+def recommendations(request):
+    return HttpResponse('This is where the FolkRank magic goes! :D')
