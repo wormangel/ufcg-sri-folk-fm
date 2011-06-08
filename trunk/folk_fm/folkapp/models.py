e429399be463c57d59d56aae48c6a195acf18fb1
wormangel <wormangel@2af12e46-81bf-4599-da0b-a47663b5e3f1>
2011-06-08 04:04:22 +0000

    Corrigidas classes do modelo

--- trunk/folk_fm/folkapp/models.py
+++ trunk/folk_fm/folkapp/models.py
@@ -3,18 +3,20 @@ from django.db import models
 # Model
 
 class User(models.Model):
-	name = models.CharField(max_length=30)
-	friends = models.ManyToManyField(User)
-	
-	def tag(self, band, tagText):
-	
-	def addFriend(self, user):
-	
+    name = models.CharField(max_length=30)
+    friends = models.ManyToManyField('self')
+
+    def tag(self, band, tagText):
+        pass
+
+    def addFriend(self, user):
+        pass
+
 class Tag(models.Model):
-	text = models.CharField(max_length=20)
+    text = models.CharField(max_length=20)
 	
 class Band(models.Model):
-	name = models.CharField(max_length=40)
+    name = models.CharField(max_length=40)
 
 class UserTagBand(models.Model):
     user = models.ForeignKey(User)
