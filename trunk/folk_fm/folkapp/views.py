886885a6235367075d229ad3a2000f91f5a6e06c
wormangel <wormangel@2af12e46-81bf-4599-da0b-a47663b5e3f1>
2011-06-09 04:13:12 +0000

    login de usuario

--- trunk/folk_fm/folkapp/views.py
+++ trunk/folk_fm/folkapp/views.py
@@ -1,9 +1,15 @@
 # Create your views here.
 from django.http import HttpResponse
 from django.shortcuts import render_to_response
+from django.template import RequestContext
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from django.contrib.auth.models import User
+from folkapp.models import UserProfile
+
 
 def index(request):
-    return render_to_response('home.html')
+    return render_to_response('home.html', context_instance=RequestContext(request))
 
 def user_profile(request, id_user):
     return HttpResponse('This is the user profile page! :D')
@@ -16,3 +22,17 @@ def bands_by_tag(request, id_tag):
 
 def recommendations(request):
     return HttpResponse('This is where the FolkRank magic goes! :D')
+
+@receiver(post_save, sender=User)
+def createUserProfile(sender, **kwargs):
+    if (kwargs['created'] == True):
+        profile = UserProfile(name=kwargs['instance'].username, user=kwargs['instance'])
+        profile.save()
+        profile.id
+
+def populate_test_db(request):
+    lucas = User(username='lucas')
+    lucas.set_password('123')
+    lucas.save()
+    lucas.id
+    return HttpResponse('Ok!')
